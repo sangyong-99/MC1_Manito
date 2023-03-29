b0db2c25e7e0563647864d3e0f0778d2be11bdbe
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-03-29 11:48:48 +0900

    nd 최종???????????

--- Teletubbies.swiftpm/End.swift
+++ Teletubbies.swiftpm/End.swift
@@ -1,49 +1,57 @@
 //
-//  End.swift
+//  Start.swift
 //  Teletubbies
 //
 //  Created by 이재혁 on 2023/03/27.
 //
 
 import SwiftUI
+import AVFoundation
 
 struct End: View {
     @State var sceneNumber = 1
-    @State var timerCount = 0
-    
-    let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
-    
-    @State var currentDate: Date = Date()
-    @State var change: Bool = true
-    @State var isTimerRunning = true
-    
-    @State var attack_xpos = 500
-    @State var attack_ypos = 250
-    @State var color_attack_xpos = 200
-    @State var color_attack_ypos = 250
-    
-    @State var rainbow_opacity = false
-    
-    
+        @State var timerCount = 0
+        
+        let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+        
+        @State var currentDate: Date = Date()
+        @State var change: Bool = true
+        @State var isTimerRunning = true
+        
+        @State var attack_xpos = 500
+        @State var attack_ypos = 250
+        @State var color_attack_xpos = 200
+        @State var color_attack_ypos = 250
+        
+        @State var rainbow_opacity = false
+
     var body: some View {
         ZStack {
             ZStack{
+                if sceneNumber == 1{
+                    Image("background").resizable().scaledToFill().ignoresSafeArea()
+                    Image("sixpeople").resizable().frame(width: CGFloat(200), height: CGFloat(80))
+                        .position(x: CGFloat(500), y: CGFloat(300))
+                    Image("rainbow").resizable().frame(width:1000, height:400)
+                        .opacity(rainbow_opacity ? 1.0 : 0.0)
+                        .animation(.easeIn(duration:2.5), value: rainbow_opacity)
+
+                }
                 
-                
-            }.background(Image("background").resizable().scaledToFill()).ignoresSafeArea()
-            .onReceive(timer, perform: { value in
+            }.onReceive(timer, perform: { value in
                 currentDate = value
+                
                 change.toggle()
                 timerCount += 1
                 if timerCount > 3 {
                     rainbow_opacity = true
+                    
                 }
-                
+                //timer.upstream.connect().cancel() 타이머 취소
             })
-            
+
             
         }
-        
     }
 }
 
