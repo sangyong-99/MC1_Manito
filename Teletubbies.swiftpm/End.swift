9600a29848cfdc90cea488ce85ad47f06ef65a41
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-03-29 11:33:32 +0900

    nd 최종??????????

--- Teletubbies.swiftpm/End.swift
+++ Teletubbies.swiftpm/End.swift
@@ -28,36 +28,22 @@ struct End: View {
     var body: some View {
         ZStack {
             ZStack{
-                if sceneNumber == 1 { // 텔레토비와 몬스터 그냥 움직이기만 하고 대기
-                    Image("rainbow").resizable().frame(width:800, height:400)
-                        .opacity(rainbow_opacity ? 1.0 : 0.0)
-                        .animation(.easeIn(duration:2.5), value: rainbow_opacity)
-                    if change {
-                        
-                    }
-                    else {
-                        
-                    }
-                }
-                else {
-                    
-                }
                 
-            }.onReceive(timer, perform: { value in
+                
+            }.background(Image("background").resizable().scaledToFill()).ignoresSafeArea()
+            .onReceive(timer, perform: { value in
                 currentDate = value
-                
                 change.toggle()
                 timerCount += 1
                 if timerCount > 3 {
                     rainbow_opacity = true
                 }
-                //timer.upstream.connect().cancel() 타이머 취소
+                
             })
             
-            Spacer()
             
         }
-        .background(Image("background_school"))
+        
     }
 }
 
